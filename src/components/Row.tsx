163f469a00263ac1fc6868b7c11dd26b404cfd2b
k <id>
2025-09-15 21:49:28 -0600

    fix custom names

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -3,11 +3,22 @@ import { Player } from '../lib/player'
 import { getRank } from '../lib/ranks'
 import { Characters } from './Characters'
 
+import playerNames from '../../config/playernames.json';
+
 interface Props {
   player: Player
 }
 
+// Custom names override what the Slippi API has
+// I put the option here for names that were inputted on the google form.
+const customNames = playerNames;
+
 export function Row({ player }: Props) {
+  let names = [player.displayName];
+  if (customNames.hasOwnProperty(player.connectCode.code)) {
+      let customName = customNames[player.connectCode.code];
+      names = customName.split('/');
+  }
 
   const codeToId = (code: string) => {
     const parts = code.split('#')
@@ -54,36 +65,46 @@ export function Row({ player }: Props) {
   const rankChange = getRankChange(player);
   const ratingChange = getRatingChange(player);
 
-  return (
-    <tr className={`${playerRank.bgClass} border-separate border-spacing-2 border-b-2 border-gray-600`} >
-      <td className="md:text-2xl text-gray-300 md:px-6 md:py-4 md:p-1 whitespace-nowrap">
-        <div>{isActive && `#${player.rankedNetplayProfile.rank}`}</div>
-        {Boolean(rankChange) && changeArrow(rankChange)} </td>
-      <td className="text-gray-100 md:px-6 md:py-4 p-1 whitespace-nowrap text-center overflow-hidden md:max-w-full max-w-[7rem] text-elipses">
-        <a className="md:text-xl text-sm max-w-xs text-gray-300 hover:text-gray-500 hover:underline" href={codeToUrlSlug(player.connectCode.code)}>{player.displayName}</a>
-        <div className="text-gray-300 text-xs">{player.connectCode.code}</div>
-      </td>
-      <td className="md:text-xl text-sm text-gray-900 md:px-6 md:py-4 p-1 whitespace-nowrap text-center">
+  if (isActive) {
+    return (
+      <tr className={`${playerRank.bgClass} border-separate border-spacing-2 border-b-2 border-gray-600`} >
+        <td className="md:text-2xl text-gray-300 md:px-6 md:py-4 md:p-1 whitespace-nowrap">
+          <div>{isActive && `#${player.rankedNetplayProfile.rank}`}</div>
+          {Boolean(rankChange) && changeArrow(rankChange)} </td>
+        <td className="text-gray-100 md:px-6 md:py-4 p-1 whitespace-nowrap text-center overflow-hidden md:max-w-full max-w-[7rem] text-elipses">
+          <a className="md:text-xl text-sm max-w-xs text-gray-300 hover:text-gray-500 hover:underline" href={codeToUrlSlug(player.connectCode.code)}>
+            {
+              names.map((name) =>
+                <p key={`uname_${name}`}>{name}</p>
+              )
+            }
+          </a>
+          <div className="text-gray-300 text-xs">{player.connectCode.code}</div>
+        </td>
+        <td className="md:text-xl text-sm text-gray-900 md:px-6 md:py-4 p-1 whitespace-nowrap text-center">
 
-        {playerRank.iconUrl && <div className="flex items-center justify-center">
-          <img className="md:h-10 md:w-10 h-6 w-6 drop-shadow" src={playerRank.iconUrl} />
-        </div>}
-        <div className="md:text-lg text-xs max-w-xs text-gray-300 uppercase">
-          {playerRank.name}
-        </div>
-        <div className="text-gray-300 md:text-sm text-xs">
-          {isActive && Math.floor(player.rankedNetplayProfile.ratingOrdinal)}
-          {isActive && Boolean(ratingChange) && changePlusMinus(ratingChange)}
-        </div>
-      </td>
-      <td className="md:text-sm text-xs text-gray-300 md:px-6 md:py-4 py-1  md:max-w-[18rem] max-w-[3rem]">
-        <Characters player={player} totalGames={totalGames} />
-      </td>
-      <td className="md:text-xl text-gray-300 text-sm md:px-6 md:py-4 md:p-1 whitespace-nowrap">
-        {Boolean(totalGames) && <><span className="text-green-500">{player.rankedNetplayProfile.wins ?? 0}</span><span className="md:p-1">/</span>
-        <span className="text-red-500">{player.rankedNetplayProfile.losses ?? 0}</span>
-      </>}
-      </td>
-    </tr>
-  );
+          {playerRank.iconUrl && <div className="flex items-center justify-center">
+            <img className="md:h-10 md:w-10 h-6 w-6 drop-shadow" src={playerRank.iconUrl} />
+          </div>}
+          <div className="md:text-lg text-xs max-w-xs text-gray-300 uppercase">
+            {playerRank.name}
+          </div>
+          <div className="text-gray-300 md:text-sm text-xs">
+            {isActive && Math.floor(player.rankedNetplayProfile.ratingOrdinal)}
+            {isActive && Boolean(ratingChange) && changePlusMinus(ratingChange)}
+          </div>
+        </td>
+        <td className="md:text-sm text-xs text-gray-300 md:px-6 md:py-4 py-1  md:max-w-[18rem] max-w-[3rem]">
+          <Characters player={player} totalGames={totalGames} />
+        </td>
+        <td className="md:text-xl text-gray-300 text-sm md:px-6 md:py-4 md:p-1 whitespace-nowrap">
+          {Boolean(totalGames) && <><span className="text-green-500">{player.rankedNetplayProfile.wins ?? 0}</span><span className="md:p-1">/</span>
+          <span className="text-red-500">{player.rankedNetplayProfile.losses ?? 0}</span>
+        </>}
+        </td>
+      </tr>
+    );
+  } else {
+    return null;
+  }
 }
